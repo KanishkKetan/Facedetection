590cf03dfde6ccd25c5bebb1c90904070c3599a1
KanishkKetan <74758854+KanishkKetan@users.noreply.github.com>
2020-11-22 18:26:50 +0530

    Update facedet.py

--- facedet.py
+++ facedet.py
@@ -1,26 +1,15 @@
 import cv2
-import sys
 
-imagePath = sys.argv[1]
-cascPath = "haarcascade_frontalface_default.xml"
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-faceCascade = cv2.CascadeClassifier(cascPath)
+img = cv2.imread('abcd.jpg')
 
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-)
-
-print("Found {0} faces!".format(len(faces)))
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
 for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+cv2.imshow('img', img)
+cv2.waitKey()
